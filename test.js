f28ce6bb0ded26aa379ecb656cf12db4e154f2f0
pepelazz <pepelazz00@gmail.com>
2015-05-15 09:50:45 +0400

    update

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./src/javascript/util.coffee":[function(require,module,exports){
-var calcWidthOrHeight, fixSize;
+var calcHandiImg, calcWidthOrHeight, fixSize;
 
 $(function() {
   fixSize();
@@ -22,6 +22,7 @@ fixSize = (function() {
     });
   }
   calcWidthOrHeight();
+  calcHandiImg();
 });
 
 calcWidthOrHeight = (function() {
@@ -49,6 +50,20 @@ calcWidthOrHeight = (function() {
       width: 'auto'
     });
   }
+});
+
+calcHandiImg = (function() {
+  var $img, $leftTitle, $topTitle, leftTitleRight, topTitleBottom;
+  $topTitle = $('.slide1 .row1 .col2 h3');
+  $leftTitle = $('.slide1 .row3 .col1');
+  topTitleBottom = $topTitle.offset().top + $topTitle.height();
+  leftTitleRight = $leftTitle.offset().left + $leftTitle.width();
+  $img = $('.img-handi img');
+  $($img).css({
+    height: window.innerHeight - topTitleBottom
+  }).css({
+    width: 'auto'
+  });
 });
 
 
